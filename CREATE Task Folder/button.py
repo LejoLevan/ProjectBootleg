e6ee855f6a6207a9b6a00e3ab1d1b7b6321b38bc
LejoLevan <JonathanVonLe@gmail.com>
2020-04-03 15:33:44 -0400

    started button class, not done yet

---
+++ CREATE Task Folder/button.py
@@ -0,0 +1,15 @@
+import pygame.font
+
+class Button:
+
+    def __init__(self,rpg,msg):
+        """Initialize button attributes."""
+        self.screen = rpg.screen
+        self.screen_rect = self.screen.get_rect()
+
+        #Set the dimensions and properties of the button
+        self.width, self.height = 200,50
+        self.button_color = (0,255,0)
+        self.text_color = (255,255,255)
+        self.font = pygame.font.SysFont(None, 48)
+        
