563044009b29ac03a9abbe195d1b33f288cec852
LejoLevan <JonathanVonLe@gmail.com>
2020-05-03 18:51:24 -0400

    minor edits based on last commit

--- CREATE Task Folder/main.py
+++ CREATE Task Folder/main.py
@@ -17,7 +17,7 @@ class RPG:
     def __init__(self):
         pygame.init()
         self.settings = Settings()
-        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height), flags=pygame.SCALED and pygame.FULLSCREEN)
+        self.screen = pygame.display.set_mode([self.settings.screen_width, self.settings.screen_height], flags=pygame.SCALED and pygame.FULLSCREEN)
         pygame.display.set_caption("Project Bootleg")
 
         self.template = Template(self)
